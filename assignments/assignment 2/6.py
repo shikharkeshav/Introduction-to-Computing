5ebe1711fd2783d301a92aa09fb7a3886ec834ee
Shikhar <shikharkeshav.bt22cseds@pec.edu.in>
2022-12-23 08:46:25 +0530

    Update 6.py

--- assignments/assignment 2/6.py
+++ assignments/assignment 2/6.py
@@ -2,7 +2,7 @@ x=int(input('Enter first side '))
 y=int(input('Enter second side '))
 z=int(input('Enter third side '))
 lst=[x,y,z]
-# lst.remove(max(x,y,z))
+lst.remove(max(x,y,z))
 if(max(x,y,z)<sum(lst)):
     print('yes')
 else:
